9389f5306124b4bddc025498317d2fc9ced96b2f
yevgen <y.kutsy86@gmail.com>
2019-08-29 12:02:01 +0200

    renamed variable $x to $weekDay, added new spaces

--- weekday.php
+++ weekday.php
@@ -19,36 +19,48 @@ $d = $day;
 $m = 0;
 $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
 $c = substr($year, 0, 2);
+
 if ($m >= 11) {
     $c = substr($year - 1, 0, 2);
 }
+
 $y = substr($year, 2, 2);
+
 if ($m >= 11) {
     $y = substr($year - 1, 2, 2);
 }
 
 $w = ($d + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
+
 if ($w == 1) {
-    $x = "Montag";
+    $weekDay = "Montag";
+
 } elseif ($w == 2) {
-    $x = "Dienstag";
+    $weekDay = "Dienstag";
+
 } elseif ($w == 3) {
-    $x = "Mittwoch";
+    $weekDay = "Mittwoch";
+
 } elseif ($w == 4) {
-    $x = "Donnerstag";
+    $weekDay = "Donnerstag";
+
 } elseif ($w == 5) {
-    $x = "Freitag";
+    $weekDay = "Freitag";
+
 } elseif ($w == 6) {
-    $x = "Samstag";
+    $weekDay = "Samstag";
+
 } elseif ($w == 0) {
-    $x = "Sonntag";
+    $weekDay = "Sonntag";
+
 } else {
     echo "Error: Unknown w={$w}\n";
     exit(1);
 }
+
 echo "Eingabe: {$day}.{$month}.{$year}\n";
 echo strftime("Berechnung PHP: Wochentag='%A'\n", strtotime("$year-$month-$day"));
-echo "Berechnung Algorithmus: Wochentag='{$x}'\n";
+echo "Berechnung Algorithmus: Wochentag='{$weekDay}'\n";
 if ($argc > 4 && ($argv[4] == '-d' || $argv[4] == '--debug')) {
     echo "DEBUG: m={$m} y={$y} c={$c}\n";
 }
