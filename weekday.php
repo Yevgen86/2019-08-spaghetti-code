9b50047faf10aae9c618d6f8cebb79b96f5b7191
yevgen <y.kutsy86@gmail.com>
2019-08-29 15:10:05 +0200

    extracted methods in 3 functions for In/Output and Debug

--- weekday.php
+++ weekday.php
@@ -15,7 +15,6 @@ if ($argc < 4 || $argc > 5) {
     exit(1);
 }
 
-$d = $day;
 $m = 0;
 $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
 $c = substr($year, 0, 2);
@@ -30,52 +29,62 @@ if ($m >= 11) {
     $y = substr($year - 1, 2, 2);
 }
 
-$w = ($d + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
+$w = ($day + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
 
-$weekDayNames = [
-    "sonntag",
-    "Montag",
-    "Dienstag",
-    "Mittwoch",
-    "Donnerstag",
-    "Freitag",
-    "Samstag",
-];
+/**
+ * @param int $w
+ * @return mixed
+ */
+function getWeekDayName(int $w)
+{
+    $weekDayNames = [
+        "sonntag",
+        "Montag",
+        "Dienstag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Samstag",
+    ];
 
-$weekDay = $weekDayNames[$w];
-
-
-/* Comment while test the new code
- * if ($w == 1) {
-	$weekDay = "Montag";
-
-} elseif ($w == 2) {
-	$weekDay = "Dienstag";
-
-} elseif ($w == 3) {
-	$weekDay = "Mittwoch";
-
-} elseif ($w == 4) {
-	$weekDay = "Donnerstag";
-
-} elseif ($w == 5) {
-	$weekDay = "Freitag";
-
-} elseif ($w == 6) {
-	$weekDay = "Samstag";
-
-} elseif ($w == 0) {
-	$weekDay = "Sonntag";
-
-} else {
-	echo "Error: Unknown w={$w}\n";
-	exit(1);
-}*/
-
-echo "Eingabe: {$day}.{$month}.{$year}\n";
-echo strftime("Berechnung PHP: Wochentag='%A'\n", strtotime("$year-$month-$day"));
-echo "Berechnung Algorithmus: Wochentag='{$weekDay}'\n";
-if ($argc > 4 && ($argv[4] == '-d' || $argv[4] == '--debug')) {
-    echo "DEBUG: m={$m} y={$y} c={$c}\n";
+    $weekDay = $weekDayNames[$w];
+    return $weekDay;
 }
+$weekDay = getWeekDayName($w);
+/**
+ * @param $day
+ * @param $month
+ * @param $year
+ */
+function printEingabe($day, $month, $year): void
+{
+    echo "Eingabe: {$day}.{$month}.{$year}\n";
+}
+/**
+ * @param $year
+ * @param $month
+ * @param $day
+ * @param $weekDay
+ */
+function printAusgabe($year, $month, $day, $weekDay): void
+{
+    echo strftime("Berechnung PHP: Wochentag='%A'\n", strtotime("$year-$month-$day"));
+    echo "Berechnung Algorithmus: Wochentag='{$weekDay}'\n";
+}
+/**
+ * @param int $m
+ * @param $y
+ * @param $c
+ */
+function printDebugOutput(int $m, $y, $c): void
+{
+    if ($argc > 4 && ($argv[4] == '-d' || $argv[4] == '--debug')) {
+        echo "DEBUG: m={$m} y={$y} c={$c}\n";
+    }
+}
+
+
+printEingabe($day, $month, $year);
+printAusgabe($year, $month, $day, $weekDay);
+printDebugOutput($m, $y, $c);
 
